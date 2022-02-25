44bad6a22e6526ae5e5f859e4c961e85aeab7797
Henry Graham Costello <hgcostello@gmail.com>
2022-02-25 09:31:30 -0500

    adding the numbers test file 'nano'

--- testcodefile.py
+++ testcodefile.py
@@ -1,2 +1,19 @@
 ## This comment indicates version 1, original version
 ## Editting, version 2 store on branch
+
+
+
+
+def get_name():
+    name = input("What is your name? ")
+    # print("Your name is {}.".format(name))
+    return name
+
+def greet_user():
+    name = get_name()
+    print("hello {}!".format(name))
+    # statement = "Nice to meet you {}!".format(name)
+    # print(statement)
+
+
+greet_user()
